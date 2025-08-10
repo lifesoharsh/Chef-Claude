a17060f533829343e6f59da3a4be8c40f54e3e09
Harsh Malik <harshmalik2003@gmail.com>
2025-08-10 18:04:41 +0530

    Prepare for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.post("/api/recipe", async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 })
-
-app.listen(3000, () => console.log("✅ Backend running at http://localhost:3000"))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`✅ Backend running on port {$PORT}`))
