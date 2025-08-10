4c22a50e9f4b2de7c38c945fda4e56c9636e0660
Harsh Malik <harshmalik2003@gmail.com>
2025-08-10 18:13:46 +0530

    fixed backend port usage display

--- backend/server.js
+++ backend/server.js
@@ -38,4 +38,4 @@ app.post("/api/recipe", async (req, res) => {
     }
 })
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`✅ Backend running on port {$PORT}`))
+app.listen(PORT, () => console.log(`✅ Backend running on port ${PORT}`))
